312541f08b9b703dd217fec4fa87ec204dc96871
EC2 Default User <ec2-user@ip-172-31-81-255.ec2.internal>
2019-11-11 08:12:28 +0000

    ボトムス一覧表示できました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-    get 'clothes' => 'clothes#index'
     root to: "home#index"
+    get 'clothes' => 'clothes#index'
+    
+    #root :to => 'コントローラ名#アクション名'
+    root :to => 'bottoms#index'
+    get 'bottoms' => 'bottoms#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
